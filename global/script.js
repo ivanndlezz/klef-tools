4041a0a50bb58970f779083442648f7bc9342e3d
Ivan Gonzalez <ivangklef@gmail.com>
2025-12-18 19:18:20 -0700

    Klef Apps | Day 2 v4

---
+++ global/script.js
@@ -0,0 +1,8 @@
+// Toggle simple del menú móvil
+document.querySelector(".hamburger").addEventListener("click", function () {
+  document.querySelector(".mega-menu").classList.toggle("open");
+});
+
+document.querySelector("#close-ham").addEventListener("click", function () {
+  document.querySelector(".mega-menu").classList.remove("open");
+});

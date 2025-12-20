830061e365d47347336fc629b3561ae8b328ae7e
Ivan Gonzalez <ivangklef@gmail.com>
2025-12-19 22:21:35 -0700

    Klef Apps | Day 3 v4.13

--- global/script.js
+++ global/script.js
@@ -13,4 +13,27 @@ function toggleMenu() {
   document.querySelector(".mega-menu").classList.toggle("open");
 }
 
-console.log("is loaded");
+//console.log("is loaded");
+
+/**/
+
+document.addEventListener("DOMContentLoaded", () => {
+  const rutas = [
+    "/",
+    "/apps/",
+    "/apps/mega-menu",
+    "/apps/modular-creatio-playground",
+    "/apps/svg-symbol-creator",
+    "/apps/ui/",
+    "/apps/ui/design-system/",
+    "/apps/ui/component-library/",
+    "/docs",
+    "/docs/ux-ui-architecture-principles.html",
+    "/docs/about-styles.html",
+  ];
+  const div = document.createElement("div");
+  div.id = "only-with-stylebot";
+  div.style.display = "none";
+  div.innerHTML = rutas.map((r) => `<a href="${r}">${r}</a>`).join("");
+  document.body.appendChild(div);
+});
